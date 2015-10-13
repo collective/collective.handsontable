3a0e8b627c34c2a7516d39ba5d708775db4841f7
Maurits van Rees <maurits@vanrees.org>
2015-10-13 15:27:22 +0300

    Read column headers from the data, if it is a dict.

--- src/collective/handsontable/js/patterns/handsontable/pattern.js
+++ src/collective/handsontable/js/patterns/handsontable/pattern.js
@@ -41,19 +41,43 @@ define([
     },
     init: function () {
       var self = this;
-      var hot = new Handsontable(self.$el[0], {
-        height: 396,
-        colHeaders: true,
+
+      var colHeaders = function(index) {
+        if (self._colHeaders) {
+          return self._colHeaders[index];
+        }
+        return index;
+      };
+
+      self.hot = new Handsontable(self.$el[0], {
+        height: 296,
+        colHeaders: colHeaders,
         rowHeaders: true,
         stretchH: 'all',
         columnSorting: true,
         contextMenu: true
       });
 
-      if (self.options.data_src){
+      if (self.options.data_src) {
         $.ajax({url: self.options.data_src}).done(
-          function(data){
-            hot.loadData(data);
+          function(data) {
+            // The data may be an array (list) or an associative array
+            // (dict).  For a list we do nothing special.  From a dict
+            // we want to use the keys as column headers.
+            if (data && !Array.isArray(data[0])) {
+              // Get the column headers.  We assume that the first
+              // dictionary has all the keys.
+              var keys = [];
+              for (var key in data[0]) {
+                if (data[0].hasOwnProperty(key)) {
+                  keys.push(key);
+                }
+              }
+              // Save the keys in a variable that the colHeaders
+              // method will use.
+              self._colHeaders = keys;
+            }
+            self.hot.loadData(data);
           });
       }
 
