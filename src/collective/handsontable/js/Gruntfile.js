3788e96c4c512ecbcd64138b0f12e22cac8a38e5
Franco Pellegrini <frapell@gmail.com>
2015-10-13 12:13:20 +0300

    Fix paths

--- src/collective/handsontable/js/Gruntfile.js
+++ src/collective/handsontable/js/Gruntfile.js
@@ -3,10 +3,10 @@ module.exports = function(grunt) {
   'use strict';
 
   // Get mockup-core's grunt infrastructure
-  var MockupGrunt = require('./bower_components/mockup/mockup/js/grunt');
+  var MockupGrunt = require('../bower_components/mockup/mockup/js/grunt');
 
   // Include the project's RequireJS configuration
-  var requirejsOptions = require('./js/config');
+  var requirejsOptions = require('./config');
 
   // Create a new insance of the Mockup grunt task suite
   var mockup = new MockupGrunt(requirejsOptions);
